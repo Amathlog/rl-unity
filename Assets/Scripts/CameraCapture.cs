99681846bc8719da830be7e9c1b421cbb9745cf0
Adrien LOGUT <adrien.logut@ensimag.grenoble-inp.fr>
2017-02-08 20:32:44 -0500

    Add env

---
+++ Assets/Scripts/CameraCapture.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraCapture : MonoBehaviour
+{
+    [HideInInspector] public Texture2D RenderResult;
+
+    private void Start()
+    {
+        RenderResult = new Texture2D(Screen.width, Screen.height);
+    }
+
+    void OnPostRender()
+    {
+        Camera owner = GetComponent<Camera>();
+        RenderTexture target = owner.targetTexture;
+
+        if (target == null)
+            return;
+
+        RenderResult = new Texture2D(target.width, target.height, TextureFormat.ARGB32, true);
+        Rect rect = new Rect(0, 0, target.width, target.height);
+        RenderResult.ReadPixels(rect, 0, 0, true);
+    }
+}
---
+++ Assets/Scripts/Environment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Environment : MonoBehaviour {
+
+    [SerializeField] private GameObject cam;
+
+    public byte[] GetFrame()
+    {
+        return Color32ArrayToByteArray(ReadScreen());
+    }
+
+    private byte[] Color32ArrayToByteArray(Color32[] colors)
+    {
+        if (colors == null || colors.Length == 0)
+            return null;
+
+        int lengthOfColor32 = Marshal.SizeOf(typeof(Color32));
+        int length = lengthOfColor32 * colors.Length;
+        byte[] bytes = new byte[length];
+
+        GCHandle handle = default(GCHandle);
+        try
+        {
+            handle = GCHandle.Alloc(colors, GCHandleType.Pinned);
+            IntPtr ptr = handle.AddrOfPinnedObject();
+            Marshal.Copy(ptr, bytes, 0, length);
+        }
+        finally
+        {
+            if (handle != default(GCHandle))
+                handle.Free();
+        }
+
+        return bytes;
+    }
+
+    private void Update()
+    {
+        //print(GetFrame()[0]);
+    }
+	
+	private Color32[] ReadScreen () {
+        Texture2D tex = cam.GetComponent<CameraCapture>().RenderResult;
+        if (tex == null)
+            return null;
+		return tex.GetPixels32();
+	}
+}
