932242f950374c988bb6b553a03e27fc5345c1a9
Adrien LOGUT <adrien.logut@ensimag.grenoble-inp.fr>
2017-02-16 20:41:31 -0500

    Add Vector3 base

---
+++ simulator/Assets/Scripts/Vector3_base.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public class Vector3_base {
+    public float x { get; set; }
+    public float y { get; set; }
+    public float z { get; set; }
+
+    public Vector3_base(Vector3 v) {
+        x = v.x;
+        y = v.y;
+        z = v.z;
+    }
+}
